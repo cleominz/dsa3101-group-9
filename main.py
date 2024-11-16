b30a6373e22ac834148d634cc5c870ae406f234d
cleo <157725074+cleominz@users.noreply.github.com>
2024-11-16 21:29:24 +0800

    Update main.py to include A1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 # Define paths to the notebooks
 notebooks = [
-    #"subgroup A/A1_guest_satisfaction_metrics.ipynb",
+    "subgroup A/A1_satisfaction_drivers.ipynb",
     "subgroup A/A2_segmentation_model.ipynb",
     "subgroup A/A3_guest_journey_patterns.ipynb",
     "subgroup B/B1_demand_forecasting_model.ipynb",
